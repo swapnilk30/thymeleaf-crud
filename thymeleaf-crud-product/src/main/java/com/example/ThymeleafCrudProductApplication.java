24ad3ccb4e4d44d2b28a5512b67a0561cfdb9f68
swapnilk30 <sk9137587676@gmail.com>
2024-07-09 10:42:51 +0530

    product inital project

---
+++ thymeleaf-crud-product/src/main/java/com/example/ThymeleafCrudProductApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymeleafCrudProductApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThymeleafCrudProductApplication.class, args);
+	}
+
+}
